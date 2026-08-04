de6fbac059803cc8cd3c29242c83765c010f6313
Malik <malik.nuriev95@gmail.com>
2026-08-04 13:10:41 +0300

    дашборд

--- src/modules/events/events.gateway.ts
+++ src/modules/events/events.gateway.ts
@@ -6,7 +6,6 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-
 @WebSocketGateway({
   cors: { origin: process.env.CORS_ORIGIN },
   namespace: '/events',
