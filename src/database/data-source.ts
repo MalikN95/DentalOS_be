758c5e8f84213d221fdd34b15a6dcc71451a0249
Malik <malik.nuriev95@gmail.com>
2026-08-05 14:28:15 +0300

    synchronize: process.env.DB_SYNC === 'true',

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -11,5 +11,5 @@ export const dataSource = new DataSource({
   database: process.env.POSTGRES_DB,
   entities: ['src/**/*.entity.ts'],
   migrations: ['src/database/migrations/*.ts'],
-  synchronize: true,
+  synchronize: process.env.DB_SYNC === 'true',
 });
